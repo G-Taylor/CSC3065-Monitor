337246cf525de90f559acf4c4212210ad50b4f73
40234272 <gtaylor18@qub.ac.uk>
2021-12-12 00:48:51 +0000

    added scheduler to carry out monitor checks every hour

--- src/app.py
+++ src/app.py
@@ -4,60 +4,67 @@ import flask
 import json
 from flask import Flask, request, render_template
 from essential_generators import DocumentGenerator
+from flask_apscheduler import APScheduler
 from datetime import datetime
 from flask_mail import Mail, Message
 from function_list import FUNCTIONS
 
+# scheduler to run the monitor checks
+sched = APScheduler()
+
 app = Flask(__name__)
 
+@sched.task("interval", id="do_monitor_checks", minutes=60, misfire_grace_time=900)
 @app.route("/")
 def home():
-    gen = DocumentGenerator()
-    results = []
-    total_time_for_monitor = 0
-    test_num = 0
-    last_ran = datetime.now()
+    with sched.app.app_context():
+        gen = DocumentGenerator()
+        results = []
+        total_time_for_monitor = 0
+        test_num = 0
+        last_ran = datetime.now()
 
-    # loop through the external functions file and test each function type with a random string
-    for func in FUNCTIONS:
-        counter = 0
-        # Do 5 tests per function
-        while counter < 5:
-            sentence = gen.sentence()   # generate a randome sentence to test the function
-            
-            # Time how long each test takes, and record total time for all functions
-            start_time = time.time()
-            requestURL = f"{FUNCTIONS[func]}/?func={func}&text={sentence}"
-            response = requests.get(requestURL)
-            end_time = time.time()
-            total = end_time - start_time
+        # loop through the external functions file and test each function type with a random string
+        for func in FUNCTIONS:
+            counter = 0
+            # Do 5 tests per function
+            while counter < 5:
+                sentence = gen.sentence()   # generate a randome sentence to test the function
+                
+                # Time how long each test takes, and record total time for all functions
+                start_time = time.time()
+                requestURL = f"{FUNCTIONS[func]}/?func={func}&text={sentence}"
+                response = requests.get(requestURL)
+                end_time = time.time()
+                total = end_time - start_time
 
-            # format the response so it can be represented as a dictionary
-            content = response.content
-            json_content = json.loads(content)
+                # format the response so it can be represented as a dictionary
+                content = response.content
+                json_content = json.loads(content)
 
-            # create result entry for each test
-            result = {
-                "function": func,
-                "test": test_num,
-                "sentence": sentence,
-                "answer": json_content['answer'],
-                "time": total,
-                "response": response.status_code,
-                "error": json_content['error']
-            }
+                # create result entry for each test
+                result = {
+                    "function": func,
+                    "test": test_num,
+                    "sentence": sentence,
+                    "answer": json_content['answer'],
+                    "time": total,
+                    "response": response.status_code,
+                    "error": json_content['error']
+                }
 
-            # Check if an alert email needs to be sent for a bad response
-            if response.status_code != 200:
-                monitor_alert(response.status_code)
-            
-            results.append(result)
-            total_time_for_monitor += total
-            counter += 1
-            test_num += 1
+                # Check if an alert email needs to be sent for a bad response
+                if response.status_code != 200:
+                    monitor_alert(response.status_code)
+                
+                results.append(result)
+                total_time_for_monitor += total
+                counter += 1
+                test_num += 1
 
-    # return the results and timing details to the index template
-    return render_template("/index.html", results=results, duration=total_time_for_monitor, last_ran=last_ran)
+        print ("scheduler working")
+        # return the results and timing details to the index template
+        return render_template("/index.html", results=results, duration=total_time_for_monitor, last_ran=last_ran)
 
 
 # method to send an email alert to desired recipients if any code other than 200 is reported
@@ -79,4 +86,6 @@ def monitor_alert(status):
 
 
 if __name__ == '__main__':
+    sched.init_app(app)
+    sched.start()
     app.run(host = '0.0.0.0', debug=True)
